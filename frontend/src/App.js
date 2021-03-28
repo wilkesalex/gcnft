9c5448a25671f4169b67c3400bceb24815f95325
amalaabraham3 <amalaabraham3@gmail.com>
2021-03-28 16:19:24 +0530

    react template setup

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <div className="bg-white my px-8 py-6 center max-w-md my-60 rounded-lg shadow-lg">
+        <div className="flex justify-center items-center">
+            <div className="px-2 py-1 bg-gray-600 text-xl text-green-100 rounded">React + Tailwind</div>
+        </div>
+        <div className="mt-4">
+            <div className="text-lg text-gray-700 font-medium" >A boilerplate code for React js with tailwindcss configured</div>
+        </div>
+    </div>
+    </div>
+
+    
+  );
+}
+
+export default App;
