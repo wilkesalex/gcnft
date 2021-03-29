ef1ec3da7a3f6a7fe4e6f9eff2ac30ddbcb994b9
amalaabraham3 <amalaabraham3@gmail.com>
2021-03-29 13:09:09 +0530

    add flow authentication and fetch token data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+import AuthCluster from './AuthCluster';
+import TokenData from './TokenData';
+import './App.css';
 
 function App() {
   return (
-    <div className="flex h-screen items-center justify-center">
-      <div className="bg-white my px-8 py-6 center max-w-md my-60 rounded-lg shadow-lg">
-        <div className="flex justify-center items-center">
-            <div className="px-2 py-1 bg-gray-600 text-xl text-green-100 rounded">React + Tailwind</div>
-        </div>
-        <div className="mt-4">
-            <div className="text-lg text-gray-700 font-medium" >A boilerplate code for React js with tailwindcss configured</div>
-        </div>
-    </div>
+    <div className="App">
+      <AuthCluster />
+      <TokenData />
     </div>
 
     
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import './styles/tailwind.css';
+import "./config"
 
 ReactDOM.render(
   <React.StrictMode>
