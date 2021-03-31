698846230ebab4b9435f8d09e5c3cabb8753ac6f
Atreya Majumdar <atreyamaj@gmail.com>
2021-03-31 12:34:39 +0530

    Trying to merge gh auth + blockchain flow

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import AuthCluster from './AuthCluster';
 import TokenData from './TokenData';
+import React, { createContext, useReducer } from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import { initialState, reducer } from "./store/reducer";
+
 import './App.css';
 
 function App() {
@@ -8,9 +14,20 @@ function App() {
     <div className="App">
       <AuthCluster />
       <TokenData />
+      <AuthContext.Provider
+      value={{
+        state,
+        dispatch
+      }}
+    >
+    <Router>
+      <Switch>
+        <Route path="/login" component={Login}/>
+        <Route path="/" component={Home}/>
+      </Switch>
+    </Router>
+    </AuthContext.Provider>
     </div>
-
-    
   );
 }
 
