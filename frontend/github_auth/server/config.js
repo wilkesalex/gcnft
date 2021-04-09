8c7cd1d5f7f01a3167041a97a4e9e9192a03c076
Alex Wilkes <1847652+wilkesalex@users.noreply.github.com>
2021-04-09 15:48:05 +0100

    Merge everything to one config file

--- frontend/github_auth/server/config.js
+++ frontend/github_auth/server/config.js
@@ -1,7 +1,7 @@
 const Joi = require("joi");
 const path = require("path");
 
-require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
+require("dotenv").config({ path: path.resolve(__dirname, "../../.env.local") });
 
 const config = {
   client_id: process.env.CLIENT_ID,
